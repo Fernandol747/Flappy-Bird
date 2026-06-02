197347455a7dc84a0af3efd1bc187806a090a2c6
Fernando Gomes Reis de Resende <fernandogomesreisderesende@gmail.com>
2026-06-02 19:16:18 -0300

    feat: movimentação do senário do jogo

--- src/FlappyBird.java
+++ src/FlappyBird.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Random;
 import javax.swing.*;
 
-public class FlappyBird extends JPanel{
+public class FlappyBird extends JPanel implements ActionListener{
     int LarguraBorda = 360;
     int AlturaBorda = 640;
 
@@ -33,19 +33,59 @@ public class FlappyBird extends JPanel{
         }
     }
 
+    // CANOS
+    int PipeX = LarguraBorda;
+    int PipeY = 0;
+    int PipeWidth = 64;
+    int PipeHeight = 512;
+
+    class Pipe {
+        int x = PipeX;
+        int y = PipeY;
+        int width = PipeWidth;
+        int height = PipeHeight;
+        Image img;
+        boolean Passed = false;
+
+        Pipe (Image img) {
+            this.img = img;
+        }
+    }
+
     // LOGICA DO JOGO
     Bird bird;
+    int VelocityX = -4;
+    int VelocityY = 0;
+    int gravity = 1;
+
+
+    ArrayList<Pipe> pipes;
+    Random random = new Random();
+
+    Timer gameLoop;
+    Timer placePipesTimer;
 
     FlappyBird(){
         setPreferredSize(new Dimension(LarguraBorda, AlturaBorda));
         setBackground(Color.blue);
 
-        backgroundImage = new ImageIcon(getClass().getResource("./flappybirdbg.png")).getImage();
-        birdImage = new ImageIcon(getClass().getResource("./flappybird.png")).getImage();
-        topPipeImage = new ImageIcon(getClass().getResource("./toppipe.png")).getImage();
-        bottomPipeImage = new ImageIcon(getClass().getResource("./bottompipe.png")).getImage();
+        backgroundImage = new ImageIcon(getClass().getResource("/flappybirdbg.png")).getImage();
+        birdImage = new ImageIcon(getClass().getResource("/flappybird.png")).getImage();
+        topPipeImage = new ImageIcon(getClass().getResource("/toppipe.png")).getImage();
+        bottomPipeImage = new ImageIcon(getClass().getResource("/bottompipe.png")).getImage();
 
         bird = new Bird(birdImage);
+          pipes = new ArrayList<Pipe>();
+
+        placePipesTimer = new Timer(1500, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                placePipes();
+            }
+        });
+        placePipesTimer.start();
+          gameLoop = new Timer (1000/60, this);
+          gameLoop.start();
     }
 
     public void paintComponent (Graphics g) {
@@ -53,12 +93,37 @@ public class FlappyBird extends JPanel{
         draw(g);
     }
 
+    public void placePipes () {
+        Pipe topPipe = new Pipe(topPipeImage);
+        pipes.add(topPipe);
+    }
+
     public void draw (Graphics g) {
         g.drawImage(backgroundImage, 0, 0, LarguraBorda, AlturaBorda, null);
 
         g.drawImage(bird.img, bird.x, bird.y, bird.width, bird.height, null);
+
+        for (int i = 0; i < pipes.size(); i++) {
+            Pipe pipe = pipes.get(i);
+            g.drawImage(pipe.img, pipe.x, pipe.y, pipe.width, pipe.height, null);
+
+        }
     }
 
+    public void move() {
+        VelocityY += gravity;
+        bird.y += VelocityY;
+        bird.y = Math.max(bird.y, 0);
 
-    
+        for (int i = 0; i < pipes.size(); i++) {
+            Pipe pipe = pipes.get(i);
+            pipe.x += VelocityX;
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        move();
+        repaint();
+    }
 }
