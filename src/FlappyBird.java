36575820f1dc9bb007fd0ad71c99f6d991475510
Fernando Gomes Reis de Resende <fernandogomesreisderesende@gmail.com>
2026-06-02 18:15:13 -0300

    feat: inicialização do projeto flappy-bird com ad imagens de fundo

---
+++ src/FlappyBird.java
@@ -0,0 +1,64 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.Random;
+import javax.swing.*;
+
+public class FlappyBird extends JPanel{
+    int LarguraBorda = 360;
+    int AlturaBorda = 640;
+
+    // IMAGEM
+
+    Image birdImage;
+    Image backgroundImage;
+    Image bottomPipeImage;
+    Image topPipeImage;
+
+    // PASSARO
+    int birdX = LarguraBorda / 8;
+    int birdY = AlturaBorda / 2;
+    int birdWidth = 34;
+    int birdHeight = 24;
+
+    class Bird {
+        int x = birdX;
+        int y = birdY;
+        int width = birdWidth;
+        int height = birdHeight;
+        Image img;
+
+        Bird(Image image) {
+            this.img = image;
+        }
+    }
+
+    // LOGICA DO JOGO
+    Bird bird;
+
+    FlappyBird(){
+        setPreferredSize(new Dimension(LarguraBorda, AlturaBorda));
+        setBackground(Color.blue);
+
+        backgroundImage = new ImageIcon(getClass().getResource("./flappybirdbg.png")).getImage();
+        birdImage = new ImageIcon(getClass().getResource("./flappybird.png")).getImage();
+        topPipeImage = new ImageIcon(getClass().getResource("./toppipe.png")).getImage();
+        bottomPipeImage = new ImageIcon(getClass().getResource("./bottompipe.png")).getImage();
+
+        bird = new Bird(birdImage);
+    }
+
+    public void paintComponent (Graphics g) {
+        super.paintComponent(g);
+        draw(g);
+    }
+
+    public void draw (Graphics g) {
+        g.drawImage(backgroundImage, 0, 0, LarguraBorda, AlturaBorda, null);
+
+        g.drawImage(bird.img, bird.x, bird.y, bird.width, bird.height, null);
+    }
+
+
+    
+}
